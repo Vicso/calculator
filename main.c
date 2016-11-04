cc0ec4e7906cf86660192e7d42563ef79954159e
Vicso <Victor.sorrenti@viacesi.fr>
2016-11-04 14:56:05 +0100

    addition

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+int a;
+int b;
 
+int addition(int a, int b)
+{
+
+    return a+b;
+}
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d\n", addition (1,4));
     return 0;
+
+
 }
